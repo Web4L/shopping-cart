4ab190f8045d73ba9fffcb5554a2a27188b77221
Daniel Hart <super.awesome.hartd@gmail.com>
2022-09-06 22:47:13 -0400

    Updated About Page and added gh pages

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,7 +2,13 @@ export function About() {
 	return (
 		<div>
 			<h1>About</h1>
-			<p>Made in (will change) hours</p>
+			<p>
+				Made in 3 Hours 10 Minutes with{" "}
+				<a href="https://youtu.be/lATafp15HWA">WDS's tutorial</a>
+			</p>
+			<p className="text-muted">
+				(minus time to setup repo and edit this :P)
+			</p>
 		</div>
 	);
 }
